56fc6fba2b271ceec002a3687eb7912f18088207
vivek0293 <vivek0293@outlook.com>
2016-04-25 18:23:22 -0500

    make it better

--- mlabs.rb
+++ mlabs.rb
@@ -33,7 +33,7 @@ begin
     puts "Enter the ID:"
     #id = gets
     #id = id.to_a
-    client[:health].find().each do |document|
+    client[:health].find().each do |row|
         puts "Query1:"
         puts "%s %s %s %s %s %s %s" % [ row['age'].to_s, row['sex'].to_s, row['race'].to_s, row['length_of_stay'].to_s, row['stay_indicator'].to_s, row['total_charges'].to_s, row['id'].to_s ]
         
