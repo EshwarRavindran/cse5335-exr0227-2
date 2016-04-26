f29c1d2571f879c7f57d87c7a5a67996afcc766e
vivek0293 <vivek0293@outlook.com>
2016-04-26 15:38:31 -0500

    make it better

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -23,7 +23,13 @@ class CatalogController < ApplicationController
              puts "%s %s %s %s %s %s %s" % [ row['age'].to_s, row['sex'].to_s, row['race'].to_s, row['length_of_stay'].to_s, row['stay_indicator'].to_s, row['total_charges'].to_s, row['id'].to_s ]
              render :json => row
          end
-
+         rescue Mongo::OperationFailure => e
+         
+         puts e.message
+         
+         ensure
+         
+         con.close if con
 
      end
      
