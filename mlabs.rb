83239be3c52ac03328643b79b3fe99e8c8ce5f99
vivek0293 <vivek0293@outlook.com>
2016-04-26 16:14:26 -0500

    make it better

--- mlabs.rb
+++ mlabs.rb
@@ -6,7 +6,7 @@ begin
     #client = Mongo::Client.new([ 'localhost:27017' ], :database => 'music')
     #mongo_client = Mongo::Client.new(['localhost'], 27017)
     #db = Mongo::Client.new(["mongo ds031912.mongolab.com:31912"]).db("heroku_mhz4f7bj")
-    client = Mongo::Client.new([ 'ds021741.mlab.com:21741' ], :database => 'heroku_n1l9hzqv', :user => 'testdb', :password => '02021993')
+    client = Mongo::Client.new([ 'ds021741.mlab.com:21741' ], :database => 'heroku_f4tlc21c', :user => 'testdb', :password => '02021993')
     puts "Connected"
     
     #coll = db.collection("testCollection")
