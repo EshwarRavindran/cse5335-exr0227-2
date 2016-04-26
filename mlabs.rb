2fe5aa545d2963646c7c8b39a803b4f9e1f74f1e
vivek0293 <vivek0293@outlook.com>
2016-04-26 02:18:22 -0500

    make it better

--- mlabs.rb
+++ mlabs.rb
@@ -17,24 +17,24 @@ begin
     #data = CSV.read('6339_Dataset_1.csv')
     #CSV.parse(data).to_json
     
-#    extracted_data   = CSV.table('6339_Dataset_1.csv')
-#    transformed_data = extracted_data.map { |row| row.to_hash }
-#    #puts transformed_data
-#    puts "file transformed"
-#    #puts transformed_data
-#    #health.drop
-#    data = client[:climate, :capped => false, :size => 2048]
-#    data.create
-#    puts "Collection Created"
-#    transformed_data.each do |data_row|
-#        #puts data_row
-#        data.insert_one(data_row)
-#    end
-#    puts "Enter the ID:"
-    #id = gets
-    #id = id.to_a
+    extracted_data   = CSV.table('6339_Dataset_1.csv')
+    transformed_data = extracted_data.map { |row| row.to_hash }
+    #puts transformed_data
+    puts "file transformed"
+    #puts transformed_data
+    #health.drop
+    data = client[:data, :capped => false, :size => 2048]
+    data.create
+    puts "Collection Created"
+    transformed_data.each do |data_row|
+        #puts data_row
+        data.insert_one(data_row)
+    end
+    puts "Enter the ID:"
+    id = gets
+    id = id.to_a
     
-    client[:climate].find().each do |row|
+    client[:data].find().each do |row|
         puts "Query1:"
         puts "%s %s %s %s %s %s %s" % [ row['latitude'].to_s, row['longitude'].to_s, row['depth'].to_s, row['mag'].to_s, row['nst'].to_s, row['gap'].to_s, row['id'].to_s ]
         
